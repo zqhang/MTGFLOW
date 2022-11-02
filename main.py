5bd1b9f607deabe65f0e87537d0796c97eec86df
zqhang <95111581+zqhang@users.noreply.github.com>
2022-11-02 16:09:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from sklearn.metrics import roc_auc_score, precision_recall_curve
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--data_dir', type=str, 
-                    default='../u_s/input/SWaT_Dataset_Attack_v0.csv', help='Location of datasets.')
+                    default='Data/input/SWaT_Dataset_Attack_v0.csv', help='Location of datasets.')
 parser.add_argument('--output_dir', type=str, 
                     default='./checkpoint/')
 parser.add_argument('--name',default='SWaT', help='the name of dataset')
