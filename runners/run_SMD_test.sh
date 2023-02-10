996ddb3f3af342287450741338f606a4df5f23c2
zqhang <95111581+zqhang@users.noreply.github.com>
2023-02-10 10:31:56 +0800

    Update run_SMD_test.sh

--- runners/run_SMD_test.sh
+++ runners/run_SMD_test.sh
@@ -3,7 +3,7 @@ do
     var=${file##*/}
     # echo $var
     echo ${var%_*}
-    CUDA_VISIBLE_DEVICES=2 python3 visual.py --name=${var%_*} --n_blocks 2 
+    CUDA_VISIBLE_DEVICES=2 python3 test.py --name=${var%_*} --n_blocks 2 
     wait
 done
 
