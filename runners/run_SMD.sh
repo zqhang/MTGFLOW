71f89bf8437040f81cce4b785f334e3f38a76733
zqhang <95111581+zqhang@users.noreply.github.com>
2023-02-10 10:32:45 +0800

    Update run_SMD.sh

--- runners/run_SMD.sh
+++ runners/run_SMD.sh
@@ -3,7 +3,7 @@ do
     var=${file##*/}
     # echo $var
     echo ${var%_*}
-    CUDA_VISIBLE_DEVICES=2 nohup python3 -u train_water_attention_1_OCC.py\
+    CUDA_VISIBLE_DEVICES=2 nohup python3 -u main.py\
         --n_blocks=2\
         --batch_size=256\
         --window_size=60\
