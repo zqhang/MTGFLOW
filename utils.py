ec87ae9c5acadcef5b5098e605b6f8d3d841db69
zqhang <95111581+zqhang@users.noreply.github.com>
2023-06-17 16:09:40 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,22 @@ def ROC(args, y_test,y_pred):
     auc=roc_auc_score(y_test,y_pred)
     print('auroc', auc)
     return auc
+class log():
+    def __init__(self) -> None:
+        self.roc_auc_max = 0
+        self.f1_max = 0
+    def print_result(self, y_test, y_pred, model, i, args):
+        y_test = np.nan_to_num(y_test)
+        y_pred = np.nan_to_num(y_pred)
+
+        auc=roc_auc_score(y_test,y_pred)
+      
+        if not os.path.exists("./{}{}modelroc".format(args.model, args.name,i, auc)):
+            os.makedirs("./{}{}modelroc/".format(args.model,args.name, i, auc))
+  
+        if self.roc_auc_max < auc:
+            self.roc_auc_max = auc
+       
+          
+        print('auroc:{:.4f}'.format(auc))
+        
